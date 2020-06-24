f0c76618ab92f7057bd30ff36ca4b660871db114
VictoriaXY6 <47672252+VictoriaXY6@users.noreply.github.com>
2020-06-24 11:15:47 -0400

    Tower

---
+++ p6/towers.cpp
@@ -0,0 +1,76 @@
+// Xing Yang
+// 5/2/2019
+
+/*
+ * CSc103 Project 6: Towers of Hanoi
+ * See readme.html for details.
+ * Please list all references you made use of in order to complete the
+ * assignment: your classmates, websites, etc.  Aside from the lecture notes
+ * and the book, please list everything.  And remember- citing a source does
+ * NOT mean it is okay to COPY THAT SOURCE.  What you submit here **MUST BE
+ * YOUR OWN WORK**.
+ * References: https://www.youtube.com/watch?v=YstLjLCGmgg
+ *
+ *
+ * Finally, please indicate approximately how many hours you spent on this:
+ * #hours: 2
+ */
+
+// TODO: write the program.
+#include <iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+#include <getopt.h> // to parse long arguments.
+#include <cstdlib> // for atoi function
+
+void hanoiTower(int n,int start, int end);
+/* Here's a skeleton main function for processing the arguments. */
+int main(int argc, char *argv[]) {
+	// define long options
+	static struct option long_opts[] = {
+		{"start",        required_argument, 0, 's'},
+		{"end",          required_argument, 0, 'e'},
+		{"num-disks",    required_argument, 0, 'n'},
+		{0,0,0,0} // this denotes the end of our options.
+	};
+	// now process the options:
+	char c; // to hold the option
+	int opt_index = 0;
+	short n=0,start=0,end=0; /* to store inputs to the towers function. */
+	while ((c = getopt_long(argc, argv, "s:e:n:", long_opts, &opt_index)) != -1) {
+		switch (c) {
+			case 's': // process option s
+				start = atoi(optarg);
+				break;
+			case 'e': // process option e
+				end = atoi(optarg);
+				break;
+			case 'n': // process option n
+				n = atoi(optarg);
+				break;
+			case '?': // this will catch unknown options.
+				// here is where you would yell at the user.
+				// although, getopt will already print an error message.
+				return 1;
+		}
+	}
+
+	/* TODO: now that you have the options and arguments,
+	 * solve the puzzle. */
+	hanoiTower(n,start,end);
+	return 0;
+}
+
+void hanoiTower(int n,int start,int end) {
+	if (n==1) {
+		cout << start << "\t" << end << endl;
+		return;
+	} else {
+		int temp = 6 - start - end;
+		hanoiTower(n - 1,start,temp);
+		cout << start << "\t" << end << endl;
+		hanoiTower(n - 1,temp,end);
+
+	}
+}
